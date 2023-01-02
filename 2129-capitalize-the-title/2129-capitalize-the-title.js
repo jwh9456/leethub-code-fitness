181ed19e9401d72a4fe2ff163f8a856a9a7da1bc
UHYEOK <49363292+jwh9456@users.noreply.github.com>
2023-01-02 23:12:16 +0900

    Time: 62 ms (96.09%), Space: 43 MB (37.79%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.js
+++ 2129-capitalize-the-title/2129-capitalize-the-title.js
@@ -4,11 +4,10 @@
  */
 var capitalizeTitle = function(title) {
     let answer = [];
-    title = title.toLowerCase()
-    let arr = title.split(" ");
+    let arr = title.toLowerCase().split(" ");
     for(let word of arr){
-        if(word.length < 3) answer.push(word);
-        else( answer.push(word[0].toUpperCase()+word.slice(1,word.length)));
+        word.length < 3? answer.push(word) : 
+        answer.push(word[0].toUpperCase()+word.slice(1,word.length))
     }
     return answer.join(" ");
 };
