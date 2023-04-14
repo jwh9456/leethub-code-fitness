2ca7f7258c7a3d51553fd85c33240aaf3dfd50cd
UHYEOK <49363292+jwh9456@users.noreply.github.com>
2023-04-15 08:55:47 +0900

    Time: 65 ms (28.75%), Space: 42.2 MB (35.83%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if (!root) return [];
+    let stack = [], res = [];
+    stack.push(root);
+    while (stack.length) {
+        let node = stack[stack.length-1];
+        if (node.left) {
+            stack.push(node.left);
+            node.left = null;
+        }
+        else if (node.right) {
+            stack.push(node.right);
+            node.right = null;
+        }
+        else res.push(stack.pop().val);
+    }
+    return res;
+};
